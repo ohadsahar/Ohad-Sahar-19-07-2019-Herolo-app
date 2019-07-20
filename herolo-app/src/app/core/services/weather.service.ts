d96fb301d1a03fe602c0d2b208040ac657fd89bb
Ohad <osahar5@gmail.com>
2019-07-20 19:01:01 +0300

    updated css

--- herolo-app/src/app/core/services/weather.service.ts
+++ herolo-app/src/app/core/services/weather.service.ts
@@ -11,7 +11,7 @@ const autoCompleteUrl = environment.autoCompleteUrl;
 const currentWeatherUrl = environment.currentWeatherUrl;
 const fiveDayWeatherUrl = environment.fiveDayWeatherUrl;
 const geoLocationWeatherUrl = environment.geoLocationWeatherUrl;
-const apiKey = 'GanYJebE4BzSUF9AqFbP6G3557SLXbI2';
+const apiKey = 'ynBMHXTCQJYPXCdT4DRxj2G4dLhdmluJ';
 
 @Injectable({ providedIn: 'root' })
 
